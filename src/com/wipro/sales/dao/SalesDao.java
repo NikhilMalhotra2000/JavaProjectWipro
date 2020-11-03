cdb4076a317fba4c82fb5109737400879d26a6e1
Ishaan Bajpai <ishaanbajpai1@gmail.com>
2020-11-03 11:58:55 +0530

    SalesDao class methods implemented

--- src/com/wipro/sales/dao/SalesDao.java
+++ src/com/wipro/sales/dao/SalesDao.java
@@ -2,10 +2,14 @@ package com.wipro.sales.dao;
 
 import com.wipro.sales.util.*;
 import java.sql.*;
-
+import java.util.ArrayList;
+import java.util.Calendar;
 import com.wipro.sales.bean.Sales;
+import com.wipro.sales.bean.SalesReport;
 
 public class SalesDao {
+	
+	Connection conn = DBUtill.getDBConnection();
 	int insertSales(Sales sales) {
 		
 		//get values to be inserted
@@ -17,19 +21,70 @@ public class SalesDao {
 		
 		
 		//get connection from DBUtill.java
-		Connection conn = DBUtill.getDBConnection();
 		try {
 			Statement st = conn.createStatement();
 			String sql = "Insert into TBL_SALES values('"+SalesID+"','"+SalesDate+"','"+ProductId+"','"+QuantitySold+"','"+salesPricePerUnit+"')";
-			st.execute(sql);
+			if(st.execute(sql))
+				return 0;
+			else
+				return -1;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Database Update failed at SalesDao.java");
 			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	String generateSalesID(java.util.Date salesDate) {
+		
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement("select SEQ_SALES_ID.nextval from dual");
+			ResultSet rs = ps.executeQuery();
+			
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(salesDate);
+			int year = cal.get(Calendar.YEAR);
+			
+			if(rs.next()) {
+				return ""+(year%100)+rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+		return "";	
+	}
+	
+	ArrayList<SalesReport> getSalesReport(){
 		
-		return 0;
+		ArrayList<SalesReport> list = new ArrayList<SalesReport>();
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement("Select * from V_SALES_REPORT");
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				SalesReport sp = new SalesReport();
+				
+				sp.setSalesID(rs.getString(1));
+				sp.setSalesDate(rs.getDate(2));
+				sp.setQuantitySold(rs.getInt(3));
+				sp.setProductUnitPrice(rs.getDouble(6));
+				sp.setSalesPricePerUnit(rs.getDouble(7));
+				sp.setProfitAmount(rs.getDouble(8));
+				
+				list.add(sp);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return list;
 		
 	}
+	
 }
